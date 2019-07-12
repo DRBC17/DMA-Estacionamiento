9b9882dc15c020c39926c0de73c0fdad3aa75f41
Diego Buezo <diegobuezo2016@gmail.com>
2019-07-12 14:40:25 -0600

    Creacion de las Ventanas IngVehiculo, Listar,RealizarPago y realizar Reporte programacion de los botones del menu y los botones del cerrar minimizar y maximizar y restaurar

---
+++ Software_Estacionamiento/Software_Estacionamiento/IngVehiculo.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Software_Estacionamiento
+{
+    /// <summary>
+    /// Lógica de interacción para IngVehiculo.xaml
+    /// </summary>
+    public partial class IngVehiculo : UserControl
+    {
+
+        public IngVehiculo()
+        {
+            InitializeComponent();
+
+
+
+        }
+        public UserControl ParentControl { get; set; }
+
+        
+        private void BtnCerrarContol_Click(object sender, RoutedEventArgs e)
+        {
+            //PanelPrincipal.Children.Remove(this);
+            
+        }
+    }
+}
